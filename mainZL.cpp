39df4ac919175a6aac032fe5c347e1f6c1a35699
Lucie Zoppi <124727560+ZOPPI-Lucie-2225087a@users.noreply.github.com>
2024-10-19 20:11:02 +0200

    Update mainZL.cpp
    en fait jai utilisé le mauvais  include de semaphore il faut utiliser le binary et pas le semaphore.h

--- mainZL.cpp
+++ mainZL.cpp
@@ -1,75 +1,64 @@
 #include <iostream>
 #include <thread>
-#include <semaphore.h>
-#include <unistd.h> //pour fct sleep()
+#include <semaphore>
 #include <mutex>
+#include <chrono>
 
 #define NB_CHAISES 5
 
-sem_t barbierPret;   //barbier pre^t
-sem_t clientPret;    //Client prêt
-sem_t clientFini;    //Client a fini
-sem_t barbierFini;   //Barbier a fini coupe de cheveux
-std::mutex accessChaises; //protection de l'accès aux chaises
+std::counting_semaphore<NB_CHAISES> chaisesDispo(NB_CHAISES);
+std::binary_semaphore barbierPret(0);
+std::binary_semaphore clientFini(0);
+std::binary_semaphore barbierFini(0);
+std::mutex mutexChaises;
 
 int clientsEnAttente = 0;
 
 void client(int id) {
+    if (chaisesDispo.try_acquire()) {
+        std::unique_lock<std::mutex> lock(mutexChaises);
+        clientsEnAttente++;
+        std::cout << "Client " << id << " attend. Clients en attente : " << clientsEnAttente << "\n";
+        lock.unlock();
 
-    std::unique_lock<std::mutex> lock(accessChaises);
+        barbierPret.release();
+        barbierFini.acquire();
 
-    if (clientsEnAttente == NB_CHAISES) {
+        std::cout << "Client " << id << " a fini la coupe.\n";
+        
+        clientFini.release();
+
+        lock.lock();
+        clientsEnAttente--;
+        std::cout << "Client " << id << " quitte. Clients en attente : " << clientsEnAttente << "\n";
+        lock.unlock();
+
+        chaisesDispo.release();
+    } else {
         std::cout << "Client " << id << " part car il n'y a pas de chaise disponible.\n";
-        return;
     }
-
-    clientsEnAttente++;
-    std::cout << "Client " << id << " attend. Clients en attente : " << clientsEnAttente << "\n";
-
-    sem_post(&clientPret);
-    lock.unlock();
-
-    sem_wait(&barbierPret);
-    std::cout << "Client " << id << " se fait couper les cheveux.\n";
-
-    sem_post(&clientFini);
-    sem_wait(&barbierFini);
-    std::cout << "Client " << id << " a terminé sa coupe de cheveux.\n";
 }
 
 void barbier() {
     while (true) {
         std::cout << "Le barbier attend des clients...\n";
-        sem_wait(&clientPret);  // Attend qu'un client arrive
+        barbierPret.acquire();
 
-        std::unique_lock<std::mutex> lock(accessChaises);
-        clientsEnAttente--;
-        std::cout << "Le barbier coupe les cheveux. Clients en attente : " << clientsEnAttente << "\n";
-        lock.unlock();
+        std::cout << "Le barbier coupe les cheveux.\n";
+        std::this_thread::sleep_for(std::chrono::seconds(6));
 
-        sem_post(&barbierPret);
-
-        sleep(6);  //délai coupe cheveux
-
-        sem_wait(&clientFini);
-        std::cout << "Le barbier a terminé la coupe.\n";
-        sem_post(&barbierFini);
+        barbierFini.release();
+        clientFini.acquire();
     }
 }
 
 int main() {
-    // init
-    sem_init(&barbierPret, 0, 0);
-    sem_init(&clientPret, 0, 0);
-    sem_init(&clientFini, 0, 0);
-    sem_init(&barbierFini, 0, 0);
-
     std::thread threadBarbier(barbier);
 
     std::thread clients[10];
     for (int i = 0; i < 10; i++) {
         clients[i] = std::thread(client, i);
-        sleep(4); //délai arrivage client
+        std::this_thread::sleep_for(std::chrono::seconds(4));
     }
 
     for (int i = 0; i < 10; i++) {
@@ -78,11 +67,6 @@ int main() {
 
     threadBarbier.join();
 
-    // destruction semaphore mais pas obligé
-    sem_destroy(&barbierPret);
-    sem_destroy(&clientPret);
-    sem_destroy(&clientFini);
-    sem_destroy(&barbierFini);
-
     return 0;
 }
+
